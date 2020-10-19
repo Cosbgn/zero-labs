c9f7128164084e281cf0bd1bb8d0661f0857659e
Costantin Bignoli <cosbgn@gmail.com>
2020-10-19 17:29:59 +0200

    added homepage redirect

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="h-screen bg-gray-900 flex justify-center items-center">
-	  <h1 class='text-3xl text-white font-bold'>Zero Labs</h1>
-	  <p class='text-gray-600'>Open Source Projects from Zero.sh</p>
+	  <div class='text-center'>
+		<h1 class='text-3xl text-white font-bold'>Zero Labs</h1>
+	  	<p class='text-gray-600'>Open Source Projects from Zero.sh</p>
+	  </div>
   </div>
 </template>
 <script>
 export default {
 	mounted(){
-		this.$router.push("https://zero.sh/labs")
+		window.location.href = "https://zero.sh/labs"
 	}
 }
 </script>
