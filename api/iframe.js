d9f5cf0b5965efec3c7c542c756f37de6bfadcfa
Costantin Bignoli <cosbgn@gmail.com>
2020-10-20 11:29:38 +0200

    added iframe function

---
+++ api/iframe.js
@@ -0,0 +1,9 @@
+exports.handler = async ( event , context ) => {
+	const {httpMethod, queryStringParameters} = event
+	if (httpMethod !== "GET") {
+		return { statusCode: 405, body: "Method Not Allowed" };
+	}
+	const embed = queryStringParameters.embed
+	const decoded = embed ? decodeURIComponent(Buffer.from(embed, 'base64').toString('binary')) : "<p style='padding:30px; font-size:2rem; font-weight:bold'>'embed' parameter missing in the url query</p>"
+	return reply.type('text/html').send(decoded)
+} 
